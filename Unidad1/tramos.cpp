2315c0160d8bc97c0cee3ccafe31b65ca2f9eace
up210676 <up210676@alumnos.upa.edu.mx>
2022-09-29 12:58:16 -0500

    Descuentos De renta

--- Unidad1/tramos.cpp
+++ Unidad1/tramos.cpp
@@ -2,22 +2,30 @@
 
 int main(int argc, char const *argv[])
 {
-    int r;
+    int r, f;
     printf ("Ingresa tu renta anual: ");
     scanf ("%d",&r);
 
 
-            if (r<=10000)
-                printf ("1era edad");
-            else 
-                if (r>=10000 && r<=20000)
-                    printf ("2da edad");
-                else
-                    if (r>=20000 && r<=35000)
-                        printf ("3ra edad");    
-                    else 
-                        if (r>=20000 && r<=35000)
-                        
+            if (r<=10000){
+                printf ("Descuento de 5 porciento");
+                f = r * 0.05;}
+            else if (r>=10000 && r<=20000){
+                    printf ("Descuento de 15 porciento");
+                    f = r * 0.15;}
+                else if (r>=20000 && r<=35000){
+                        printf ("Descuento de 20 porciento"); 
+                        f = r * 0.20;}   
+                    else if (r>=35000 && r<=60000){
+                            printf ("Descuento de 30 porciento");
+                            f = r * 0.30;} 
+                        else if (r>=60000){
+                              printf ("Descuento de 45 porciento");
+                              f = r * 0.45;} 
 
+            printf("\n");            
+            printf ("Tu renta con descuento es: %i",r-f);
+
+        
     return 0;
 }
